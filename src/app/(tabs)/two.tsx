05c5d1534d92ddce29317fa72bea2535f3779a6c
recondemon <recondemon@outlook.com>
2025-07-25 09:30:47 -0700

    fixed tab 2 style

--- src/app/(tabs)/two.tsx
+++ src/app/(tabs)/two.tsx
@@ -1,7 +1,11 @@
 
+import { getColors } from "@/src/constants/Colors";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function TabTwoScreen() {
+  const colors = getColors();
+  const styles = createStyles(colors);
+  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
@@ -10,17 +14,20 @@ export default function TabTwoScreen() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  separator: {
+const createStyles = (colors: ReturnType<typeof getColors>) =>
+  StyleSheet.create({
+    container: {
+      flex: 1,
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: colors.bg,
+    },
+    title: {
+      fontSize: 20,
+      fontWeight: "bold",
+      color: colors.text,
+    },
+    separator: {
     marginVertical: 30,
     height: 1,
     width: "80%",
